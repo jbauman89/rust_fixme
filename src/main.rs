7395d505046a25c8729daf07820fc533d18ab69e
jbauman89 <46249585+jbauman89@users.noreply.github.com>
2022-11-29 16:05:02 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,18 @@
 use std::fmt::{self, Formatter, Display};
 
-/* Demonstrates printing of a user defined struct using println! macro.*/
-
 struct City {
     name: &'static str,
-    // Latitude
     lat: f32,
-    // Longitude
     lon: f32,
 }
 
 impl Display for City {
-    // `f` is a buffer, this method must write the formatted string into it
+
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
         let lat_c = if self.lat >= 0.0 { 'N' } else { 'S' };
         let lon_c = if self.lon >= 0.0 { 'E' } else { 'W' };
 
-        // `write!` is like `format!`, but it will write the formatted string into a buffer (the first argument)
+
         write!(f, "{}: {:.3}°{} {:.3}°{}",
                self.name, self.lat.abs(), lat_c, self.lon.abs(), lon_c)
     }
@@ -29,6 +25,14 @@ struct Color {
     blue: u8,
 }
 
+impl Display for Color {
+
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "Color ({0}, {1}, {2})",
+               self.red, self.green, self.blue)
+    }
+}
+
 fn main() {
     for city in [
         City { name: "Glassboro", lat: 39.702892, lon: -75.111839 },
@@ -37,13 +41,11 @@ fn main() {
     ].iter() {
         println!("{}", *city);
     }
-
     for color in [
         Color { red: 128, green: 255, blue: 90 },
         Color { red: 0, green: 3, blue: 254 },
         Color { red: 0, green: 0, blue: 0 },
     ].iter() {
-        // Hint : Fix the code so you can print it using {}
-        println!("{:?}", *color);
+        println!("{}", *color)
     }
 }
